8191ba6444f8ca2199c2eae42e3f65a70b85e7f2
websayt <80636766+websayt@users.noreply.github.com>
2021-03-14 23:34:34 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*function initMap() {
+  var uluru = {lat: 40.5397334, lng: 44.6951272};
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 7, center: uluru});
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    
+ 
